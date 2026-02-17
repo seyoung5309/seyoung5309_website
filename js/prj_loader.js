9547851899ec4a00ba76b55280eb6db31a38257c
오세영 <leenight5309@naver.com>
2026-02-18 07:45:15 +0900

    feat: 프로젝트 세부 파일 추가

--- js/prj_loader.js
+++ js/prj_loader.js
@@ -1,20 +1,25 @@
 async function loadPosts() {
-  const listRes = await fetch('../json/post/index.json');
+  const listRes = await fetch("../json/post/index.json");
   const list = await listRes.json();
 
-  const container = document.getElementById('prj_s');
+  const container = document.getElementById("prj_s");
 
   for (const file of list) {
     const res = await fetch(`../json/post/${file}`);
     const post = await res.json();
 
-    const el = document.createElement('article');
+    const el = document.createElement("article");
     el.innerHTML = `
       <div class="prj" tag-category="${post.category}">
         <div class="img"></div>
         <p class="text">${post.title}</p>
       </div>
     `;
+
+    el.addEventListener("click", () => {
+      location.href = `./project_view.html?id=${post.id}`;
+    });
+
     container.appendChild(el);
   }
 }
---
+++ js/project_view.js
@@ -0,0 +1,28 @@
+const params = new URLSearchParams(location.search);
+const id = Number(params.get('id'));
+
+async function loadProject() {
+  const listRes = await fetch('../json/post/index.json');
+  const list = await listRes.json();
+
+  for (const file of list) {
+    const res = await fetch(`../json/post/${file}`);
+    const post = await res.json();
+
+    if (post.id === id) {
+      render(post);
+      break;
+    }
+  }
+}
+
+function render(post) {
+  const box = document.getElementById('project_view');
+  box.innerHTML = `
+    <h1>${post.title}</h1>
+    <p>${post.desc ?? ""}</p>
+    <div>${post.content}</div>
+  `;
+}
+
+loadProject();
