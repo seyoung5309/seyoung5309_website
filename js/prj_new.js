66aed25ea41bfc652319e7b6ff261b542f3f07f3
오세영 <leenight5309@naver.com>
2026-03-12 07:19:38 +0900

    chore: 가장 최근 프로젝트(진행중)

---
+++ js/prj_new.js
@@ -0,0 +1,24 @@
+const PROJECT_LIST_PATH = "../json/post/index.json";
+
+async function loadLatestProject() {
+  const listRes = await fetch(PROJECT_LIST_PATH);
+  const projectPaths = await listRes.json();
+
+  const firstPath = projectPaths[0];
+  const projectRes = await fetch(`../json/post/${firstPath.replace("./", "")}`);
+  const project = await projectRes.json();
+
+  const container = document.querySelector(".prj_main");
+  container.innerHTML = `
+    <div class="prj_card" onclick="goToProject(${project.id})">
+      <img src="${project.img}" alt="${project.title}" />
+      <p class="text">${project.title}</p>
+    </div>
+  `;
+}
+
+function goToProject(id) {
+  location.href = `project_view.html?id=${id}`;
+}
+
+loadLatestProject();
